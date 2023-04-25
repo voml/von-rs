831c0c54fb925559e0e69e4e7fe2d8634152fd63
Aster <aster@vers.site>
2023-04-25 12:25:46 +0800

    Define a unified error type

--- projects/von-core/src/errors/mod.rs
+++ projects/von-core/src/errors/mod.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fmt::{Display, Formatter};
+use serde::de::{Expected, Unexpected};
 
 #[derive(Clone, Debug)]
 pub struct VonError {
@@ -28,6 +29,34 @@ impl serde::ser::Error for VonError {
 
 impl serde::de::Error for VonError {
     fn custom<T>(msg: T) -> Self where T: Display {
+        todo!()
+    }
+
+    fn invalid_type(unexp: Unexpected, exp: &Expected) -> Self {
+        todo!()
+    }
+
+    fn invalid_value(unexp: Unexpected, exp: &Expected) -> Self {
+        todo!()
+    }
+
+    fn invalid_length(len: usize, exp: &Expected) -> Self {
+        todo!()
+    }
+
+    fn unknown_variant(variant: &str, expected: &'static [&'static str]) -> Self {
+        todo!()
+    }
+
+    fn unknown_field(field: &str, expected: &'static [&'static str]) -> Self {
+        todo!()
+    }
+
+    fn missing_field(field: &'static str) -> Self {
+        todo!()
+    }
+
+    fn duplicate_field(field: &'static str) -> Self {
         todo!()
     }
 }
